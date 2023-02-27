e5f38948ad97a9966fc9201394188dfdcabfa03e
Randy Chu <randychu7@gmail.com>
2023-02-27 12:28:49 -0600

    finished adding

--- ContactsClasses/ContactPath.java
+++ ContactsClasses/ContactPath.java
@@ -4,24 +4,29 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+
+import static java.nio.file.Files.readAllLines;
+import static java.nio.file.Files.write;
 
 public class ContactPath {
-    public static String contacts = "contacts.txt";
+    private static String contacts = "contacts.txt";
+    private static Path fileLocation = Paths.get(contacts);
 
-    public static Path contactLocation(){
-        Path fileName = Paths.get(contacts);
-
-        try{
-            Path directoryCreation = Files.createFile(fileName);
-        }catch(IOException ixo){
-            System.out.println(ixo);
-        };
-        return fileName;
+    public static Path contactLocation() throws IOException{
+        Path directoryCreation = Files.createFile(fileLocation);
+        return fileLocation;
     }
 
-    public static void main(String[] args) {
-        contactLocation();
+    public static void writer(List<String> info) throws IOException {
+        write(fileLocation, info);
     }
+
+    public static List<String> reader() throws IOException{
+        List<String>result = Files.readAllLines(fileLocation);
+        return result;
+    }
+
 
 
 
